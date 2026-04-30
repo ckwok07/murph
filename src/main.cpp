28acd04c80df93ecb90495e6b86356b7beb909e4
ckwok07 <christiankkwok910@gmail.com>
2026-04-30 20:14:00 +0800

    test GLFW window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-    std::cout << "test";
+    if (!glfwInit()) {
+        std::cerr << "failed\n";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "murph", nullptr, nullptr);
+
+    if (!window) {
+        std::cerr << "failed\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
